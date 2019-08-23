40b60c852b1f6d0430a2056d30be780f9b87e08d
Leonel <leosmac@Leonels-MacBook-Pro.local>
2019-08-23 14:16:54 -0700

    initail commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/movies',
+    {useNewUrlParser: true, useCreateIndex: true}
+);
+
+var db = mongoose.connection;
+ 
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
